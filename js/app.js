f97e49f0480ee35b98463b26d470e04c0b636412
Mariana Boesch Tavora Pacheco <182200200@senacinformatica.br>
2022-07-19 15:01:46 -0300

    criação de objetos

--- js/app.js
+++ js/app.js
@@ -31,13 +31,45 @@ const btnCancelarReceita = document.querySelector('.btn-cancelar-receita');
 btnCancelarDespesa.addEventListener('click', switchModalDespesa);
 btnCancelarReceita.addEventListener('click', switchModalReceita);
 
-
+// criação de objeto
+const financas = {
+    saldo: 10,
+    transacoes: [
+      {
+        descricao: 'Salgado na faculdade',
+        categoria: 'Despesa',
+        valor: 5.5
+      },
+      {
+        descricao: 'Livro Clean Code',
+        categoria: 'Despesa',
+        valor: 50
+      },
+      {
+        descricao: 'Grana do estágio',
+        categoria: 'Receita',
+        valor: 80
+      },
+      {
+        descricao: 'Capinha pro celular',
+        categoria: 'Despesa',
+        valor: 15
+      },
+    ]
+  };
+  
 
 // função para capturar valores e adicionar receita e despesa
 const adicionarReceita = () => {
     let nomeReceita = document.getElementById('nome-receita').value;
     let valorReceita = parseFloat(document.getElementById('valor-receita').value.replace(",", "."));
-    console.log(valorReceita)
+    const receita = {
+        descricao: nomeReceita,
+        valor: valorReceita,
+        categoria:'Receita'
+    }
+    financas.transacoes.push(receita);
+    financas.saldo = financas.saldo + valorReceita
 
     document.querySelector('.modal-receita').style.display = 'none';
 }
@@ -45,7 +77,15 @@ const adicionarReceita = () => {
 const adicionarDespesa = () => {
     let nomeDespesa= document.getElementById('nome-despesa').value;
     let valorDespesa = parseFloat(document.getElementById('valor-despesa').value.replace(",", "."));
-
+    const despesa = {
+        descricao: nomeDespesa,
+        valor: valorDespesa,
+        categoria:'Despesa'
+    }
+    financas.transacoes.push(despesa);
+    financas.saldo = financas.saldo - valorDespesa
 
     document.querySelector('.modal-despesa').style.display = 'none';
 }
+
+console.log()
