0784870cc77785c2a277bddf10857a9efb798da0
Mariana Boesch Tavora Pacheco <182200200@senacinformatica.br>
2022-07-21 16:25:27 -0300

    adicionando transações

--- js/app.js
+++ js/app.js
@@ -31,6 +31,10 @@ const btnCancelarReceita = document.querySelector('.btn-cancelar-receita');
 btnCancelarDespesa.addEventListener('click', switchModalDespesa);
 btnCancelarReceita.addEventListener('click', switchModalReceita);
 
+function formatarValor(valor) {
+  return valor.toLocaleString('pt-br', { style: 'currency', currency: 'BRL' });
+}
+
 // criação de objeto
 const financas = {
     saldo: 10,
@@ -58,7 +62,25 @@ const financas = {
     ]
   };
   
+const setSaldo = () => {
+  document.getElementById('saldo').innerHTML = `Saldo: ${formatarValor(financas.saldo)}`
+}
+setSaldo()
 
+const adicionarTransacoes = () => {
+  let tabela = '';
+  financas.transacoes.forEach(transacao => {
+    let linha = `<tr>
+                    <td class="coluna-descricao">${transacao.descricao}</td>
+                    <td class="coluna-categoria">${transacao.categoria}</td>
+                    <td class="coluna-valor">${formatarValor(transacao.valor)}</td>
+                  </tr>`
+    tabela += linha
+  });
+  document.getElementById('lista-transacoes-conteudo').innerHTML = tabela;
+  
+}
+adicionarTransacoes()
 // função para capturar valores e adicionar receita e despesa
 const adicionarReceita = () => {
     let nomeReceita = document.getElementById('nome-receita').value;
@@ -70,13 +92,16 @@ const adicionarReceita = () => {
     }
     financas.transacoes.push(receita);
     financas.saldo = financas.saldo + valorReceita
-
+    setSaldo()
     document.querySelector('.modal-receita').style.display = 'none';
+    document.getElementById('nome-receita').value = ''
+    document.getElementById('valor-receita').value = '';
 }
 
 const adicionarDespesa = () => {
     let nomeDespesa= document.getElementById('nome-despesa').value;
-    let valorDespesa = parseFloat(document.getElementById('valor-despesa').value.replace(",", "."));
+    let valorDespesa = parseFloat(document.getElementById('valor-despesa').value.replace(",", ".")); 
+    
     const despesa = {
         descricao: nomeDespesa,
         valor: valorDespesa,
@@ -84,6 +109,17 @@ const adicionarDespesa = () => {
     }
     financas.transacoes.push(despesa);
     financas.saldo = financas.saldo - valorDespesa
+  
+    setSaldo()
 
     document.querySelector('.modal-despesa').style.display = 'none';
+    document.getElementById('nome-despesa').value = ''
+    document.getElementById('valor-despesa').value = ''
 }
+
+// Adicionar função de adicionar transação aos respectivos botões
+document.querySelector('.btn-adicionar-despesa').addEventListener('click',adicionarTransacoes)
+document.querySelector('.btn-adicionar-receita').addEventListener('click',adicionarTransacoes)
+
+
+console.log(financas)
